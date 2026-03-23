2303e0ae2a3f8c9f66150620316651882c6c5ccc
ybouret <yann.bouret@gmail.com>
2026-03-23 09:19:53 +0100

    wip

--- src/root/tests/concurrent/memory.cpp
+++ src/root/tests/concurrent/memory.cpp
@@ -6,6 +6,8 @@
 #include "y/concurrent/thread.hpp"
 #include "y/memory/auto-built.hpp"
 #include "y/calculus/alignment.hpp"
+#include "y/core/rand.hpp"
+#include "y/check/crc32.hpp"
 
 using namespace Yttrium;
 
@@ -13,21 +15,75 @@ using namespace Yttrium;
 
 namespace
 {
-    static inline
-    void MemoryInThread(void * const)
+    struct Block
     {
-        Lockable &sync = Lockable::Giant();
+        void * addr;
+        size_t size;
+    };
+
+    static inline
+    void Acquire(Memory::Allocator & alloc,
+                 const size_t        nmax,
+                 Block               blocks[],
+                 size_t            & count,
+                 Core::Rand        & ran)
+    {
+        while(count<nmax)
+        {
+            Block &b = blocks[count];
+            b.size   = ran.in<size_t>(0,1000);
+            b.addr   = alloc.acquire(b.size);
+            ++count;
+        }
+    }
+
+    static inline
+    void Release(Memory::Allocator &alloc,
+                 const size_t       nmin,
+                 Block              blocks[],
+                 size_t            &count) noexcept
+    {
+        while(count>nmin)
+        {
+            Block &b = blocks[--count];
+            alloc.release(b.addr,b.size);
+        }
+    }
+
+    static inline
+    void MemoryInThread(void * const args)
+    {
+        assert(0!=args);
+        Lockable         & sync   = Lockable::Giant();
+        System::WallTime & chrono = *static_cast<System::WallTime *>(args);
+        long               seed   = 0;
         {
             Y_Lock(sync);
-            (std::cerr << "In Thread!" << std::endl).flush();
+            const uint64_t u = System::WallTime::Ticks();
+            seed = (long) CRC32::Run(u);
+            (std::cerr << "In Thread, seed=" << seed << std::endl).flush();
         }
+
+        Concurrent::Nucleus &nucleus = Concurrent::Nucleus::Location();
+        Core::Rand           ran(seed);
+        {
+            Y_Lock(sync);
+            chrono.waitFor(0.01f + ran() * 0.02f);
+        }
+        Block        blocks[100];
+        const size_t nblock = Y_Static_Size(blocks);
+        size_t       count  = 0;
+
+        Acquire(nucleus,nblock,blocks,count,ran);
+
+        Release(nucleus,0,blocks,count);
     }
 
     class MyThread : public Concurrent::Thread
     {
     public:
-        explicit MyThread() :
-        Concurrent::Thread(MemoryInThread,0)
+        explicit MyThread(System::WallTime &chrono) :
+        Concurrent::Thread(MemoryInThread,&chrono)
         {
         }
 
@@ -45,7 +101,8 @@ Y_UTEST(concurrent_memory)
 {
     const size_t numThreads = 2;
     void *       wksp[ Alignment::WordsGEQ<numThreads*sizeof(MyThread)>::Count ];
-    Memory::AutoBuilt<MyThread> threads(wksp,numThreads);
+    System::WallTime chrono;
+    Memory::AutoBuilt<MyThread> threads(wksp,numThreads,chrono);
 }
 Y_UDONE()
 
