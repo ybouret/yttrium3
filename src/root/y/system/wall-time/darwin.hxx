43fe74c79a111b1811a03e12c1f950d4c4409531
ybouret <yann.bouret@gmail.com>
2026-03-28 13:48:24 +0100

    wip

--- src/root/y/system/wall-time/darwin.hxx
+++ src/root/y/system/wall-time/darwin.hxx
@@ -1,56 +1,28 @@
 
+extern "C" {
+    uint64_t    Yttrium_mach_absolute_time();
+    long double Yttrium_mach_calibrate(int * const);
 
-#include <mach/mach.h>
-#include <mach/mach_time.h>
+}
 
 namespace Yttrium
 {
     namespace System
     {
 
-#if 0
-        class WallTime:: Code : public Object
-        {
-        public:
-            explicit Code() : Object(), freq( Calibrate() )
-            {
-            }
-
-            virtual ~Code() noexcept
-            {
-            }
-
-            inline long double convert(const uint64_t ticks) const noexcept
-            {
-                return static_cast<long double>(ticks) * freq;
-            }
-
-            const long double freq;
-
-        private:
-            Y_Disable_Copy_And_Assign(Code);
-            static inline long double Calibrate()
-            {
-                static const long double nano = 1.0e-9L;
-                Y_Giant_Lock();
-                mach_timebase_info_data_t timebase;
-                const kern_return_t       err = mach_timebase_info(&timebase);
-                if(KERN_SUCCESS!=err)
-                {
-                    throw Mach::Exception(err,"mach_timebase_info");
-                }
-                const long double  conversion_factor = static_cast<long double>(timebase.numer) / timebase.denom;
-                return nano * conversion_factor;
-
-            }
-        };
-
-#endif
-
 
         long double WallTime:: Calibrate()
         {
-            static const long double nano = 1.0e-9L;
+
+            Y_Giant_Lock();
+            int               err = 0;
+            const long double res = Yttrium_mach_calibrate(&err);
+            if(res<0.0L)
+                throw Mach::Exception(err,"mach_timebase_info");
+            return res;
+
+#if 0
+            static const long double nano = 1.0e-9L
             Y_Giant_Lock();
             mach_timebase_info_data_t timebase;
             const kern_return_t       err = mach_timebase_info(&timebase);
@@ -60,11 +32,12 @@ namespace Yttrium
             }
             const long double  conversion_factor = static_cast<long double>(timebase.numer) / timebase.denom;
             return nano * conversion_factor;
+#endif
         }
 
         uint64_t WallTime:: Ticks()
         {
-            return mach_absolute_time();
+            return Yttrium_mach_absolute_time();
         }
 
     }
---
+++ src/root/y/system/wall-time/mach.c
@@ -0,0 +1,42 @@
+
+#include "y/config/platform.h"
+
+#if defined(Y_Darwin)
+
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+
+
+uint64_t Yttrium_mach_absolute_time()
+{
+    return mach_absolute_time();
+}
+
+long double Yttrium_mach_calibrate(int * const err)
+{
+    static const long double  nano = 1.0e-9L;
+    mach_timebase_info_data_t timebase;
+    const kern_return_t       errCode = mach_timebase_info(&timebase);
+    if(KERN_SUCCESS != errCode)
+    {
+        *err = errCode;
+        return -1.0L;
+    }
+    else
+    {
+        const long double  conversion_factor = (long double)(timebase.numer) / timebase.denom;
+        return nano * conversion_factor;
+    }
+}
+
+
+#else
+
+
+uint64_t Yttrium_mach_absolute_time()
+{
+    return 0;
+}
+
+#endif
+
