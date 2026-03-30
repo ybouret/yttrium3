e640201c90065db720ba9b2883f9f6b6d53ccc83
Yann Bouret <yann.bouret@unice.fr>
2026-03-30 09:39:11 +0200

    working on base64

--- src/root/tests/ascii/base64.cpp
+++ src/root/tests/ascii/base64.cpp
@@ -1,4 +1,5 @@
 #include "y/ascii/base64.hpp"
+#include "y/libc/block/zero.h"
 #include "y/utest/run.hpp"
 
 using namespace Yttrium;
@@ -24,15 +25,45 @@ namespace
 
 Y_UTEST(ascii_base64)
 {
-	print62();
-	std::cerr << "'+','/'" << std::endl;
-	
-	std::cerr << std::endl;
+    if(false)
+    {
+        print62();
+        std::cerr << "'+','/'" << std::endl;
 
-	print62();
-	std::cerr << "'-','_'" << std::endl;
+        std::cerr << std::endl;
 
-	std::cerr << std::endl;
+        print62();
+        std::cerr << "'-','_'" << std::endl;
 
+        std::cerr << std::endl;
+    }
+
+    if(true)
+    {
+        unsigned i = 0;
+        for(unsigned j=0;j<16;++j)
+        {
+            for(unsigned k=0;k<16;++k,++i)
+            {
+                const char c = (char)i;
+                std::cerr << std::setw(3) << (unsigned) ASCII::Base64::Encoding(c);
+                if(i<255) std::cerr << ',';
+            }
+            std::cerr << std::endl;
+        }
+    }
+
+    char out[8];
+    Y_BZero(out);
+    ASCII::Base64::Encode1(out,'A');
+    std::cerr << out << std::endl;
+
+    Y_BZero(out);
+    ASCII::Base64::Encode2(out,'A','K');
+    std::cerr << out << std::endl;
+
+    Y_BZero(out);
+    ASCII::Base64::Encode3(out,'A','K','Z');
+    std::cerr << out << std::endl;
 }
 Y_UDONE()
--- src/root/y/ascii/base64.hpp
+++ src/root/y/ascii/base64.hpp
@@ -12,10 +12,16 @@ namespace Yttrium
 
         struct Base64
         {
-            static const char STD[64];
-            static const char URL[64];
-            static const char Pad = '=';
-            static void Encode1(char * const out, const uint8_t b0, const bool pad = true, const char * const table = STD) noexcept;
+            static const char    STD[64];
+            static const char    URL[64];
+            static const char    Pad = '=';
+            static const uint8_t Byte[256];
+
+            static uint8_t Encoding(const char C) noexcept;
+            static void    Encode1(char * const out, const uint8_t b0, const bool pad = true, const char * const table = STD) noexcept;
+            static void    Encode2(char * const out, const uint8_t b0, const uint8_t b1, const bool pad = true, const char * const table = STD) noexcept;
+            static void    Encode3(char * const out, const uint8_t b0, const uint8_t b1, const uint8_t b2, const char * const table = STD) noexcept;
+
         };
     }
 }
