76e4728464a2b08f46bbdad862eed74b193c669d
ybouret <yann.bouret@gmail.com>
2026-04-09 07:22:53 +0200

    basic iterator test

--- src/root/tests/container/iter.cpp
+++ src/root/tests/container/iter.cpp
@@ -7,7 +7,14 @@ using namespace Yttrium;
 
 Y_UTEST(container_iter)
 {
-    
+    int arr[7] = { 1,2,3,4,5,6,7 };
+    {
+        Iter::Linear<Iter::Forward,int>       curr(arr);
+        const Iter::Linear<Iter::Forward,int> last(arr+Y_Static_Size(arr));
+        Iter::Test::Run(curr,last);
+        
+    }
+
 }
 Y_UDONE()
 
