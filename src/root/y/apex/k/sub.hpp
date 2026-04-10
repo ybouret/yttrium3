3850c12f9264ebd2f6bf06dbd57d7f61718f9da1
ybouret <yann.bouret@gmail.com>
2026-04-10 14:51:31 +0200

    working on subtraction

--- src/root/y/apex/k/sub.hpp
+++ src/root/y/apex/k/sub.hpp
@@ -6,7 +6,7 @@
 
 #include "y/apex/k/keg.hpp"
 #include "y/check/static.hpp"
-
+#include "y/type/ints.hpp"
 
 #if defined(Y_Apex_Trace)
 #include "y/system/wall-time.hpp"
@@ -30,7 +30,26 @@ namespace Yttrium
         public:
             static uint64_t Trace; //!< optional tracing
 
-            
+            //! compute sum
+            /**
+             \param lhs lhs words
+             \param nlw lhs size
+             \param rhs rhs words
+             \param nrw rhs size
+             \return sum
+             */
+            template <typename WORD, typename CORE> static inline
+            Keg<WORD> * Compute(const WORD * const lhs,
+                                const size_t       nl,
+                                const WORD * const rhs,
+                                const size_t       nr)
+            {
+                Y_STATIC_CHECK(sizeof(WORD)<sizeof(CORE),BadSizes);
+                typedef typename SignedFor<sizeof(CORE)>::Alias::Type CarryType;
+
+                
+
+            }
 
         };
 
