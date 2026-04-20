e18ba9559f9d67f39c023a7323a4c1ce09b47178
ybouret <yann.bouret@gmail.com>
2026-04-20 12:04:22 +0200

    added some doc

--- src/root/y/container/contiguous/ro.hpp
+++ src/root/y/container/contiguous/ro.hpp
@@ -1,4 +1,3 @@
-
 //! \file
 
 #ifndef Y_ReadOnlyContiguous_Included
@@ -9,53 +8,72 @@
 
 namespace Yttrium
 {
-
+    //__________________________________________________________________________
+    //
+    //
+    //
+    //! Read Only Contiguous
+    //
+    //
+    //__________________________________________________________________________
     template <typename READABLE>
     class ReadOnlyContiguous : public READABLE
     {
     public:
-        typedef typename READABLE::ConstType ConstType;
+        //______________________________________________________________________
+        //
+        //
+        // Definitions
+        //
+        //______________________________________________________________________
+        typedef typename READABLE::ConstType ConstType; //!< alias
         using READABLE::size;
         using READABLE::ask;
 
-        typedef Iter::Linear<Iter::Forward,ConstType> ConstIterator;
-        typedef Iter::Linear<Iter::Reverse,ConstType> ConstReverseIterator;
+        typedef Iter::Linear<Iter::Forward,ConstType> ConstIterator;        //!< alias
+        typedef Iter::Linear<Iter::Reverse,ConstType> ConstReverseIterator; //!< alias
 
+        //______________________________________________________________________
+        //
+        //
+        // C++
+        //
+        //______________________________________________________________________
+
+        //! setup
         inline explicit ReadOnlyContiguous() : READABLE()
         {
         }
 
+        //! cleanup
         inline virtual ~ReadOnlyContiguous() noexcept
         {
         }
 
+        //______________________________________________________________________
+        //
+        //
+        // Methods
+        //
+        //______________________________________________________________________
+
+        //! \return legacy access
         inline ConstType * operator()(void) const noexcept
         {
             return (size()>0) ? & ask(1) : 0;
         }
 
-        inline ConstIterator begin() const noexcept {
-            return (size()>0) ? & ask(1) : 0;
-        }
+#if !defined(DOXYGEN_SHOULD_SKIP_THIS)
 
-        inline ConstIterator end() const noexcept
-        {
-            const size_t n = size();
-            return n>0 ? (&ask(1))+n : 0;
-        }
+        inline ConstIterator        begin()  const noexcept { return (size()>0) ? & ask(1) : 0; }
+        inline ConstIterator        end()    const noexcept { const size_t n = size(); return n>0 ? (&ask(1))+n : 0; }
+        inline ConstReverseIterator rbegin() const noexcept { const size_t n = size(); return (n>0) ? (&ask(n)) : 0; }
+        inline ConstReverseIterator rend()   const noexcept { return (size() > 0) ? ((&ask(1))-1) : 0; }
 
-        inline ConstReverseIterator rbegin() const noexcept {
-            const size_t n = size();
-            return (n>0) ? (&ask(n)) : 0;
-        }
-
-        inline ConstReverseIterator rend() const noexcept
-        {
-            return (size() > 0) ? ((&ask(1))-1) : 0;
-        }
+#endif // !defined(DOXYGEN_SHOULD_SKIP_THIS)
 
     private:
-        Y_Disable_Copy_And_Assign(ReadOnlyContiguous);
+        Y_Disable_Copy_And_Assign(ReadOnlyContiguous); //!< discarded
     };
 
 }
--- src/root/y/container/contiguous/rw.hpp
+++ src/root/y/container/contiguous/rw.hpp
@@ -11,87 +11,88 @@
 
 namespace Yttrium
 {
-
+    //__________________________________________________________________________
+    //
+    //
+    //
+    //! Read Write Contiguous
+    //
+    //
+    //__________________________________________________________________________
     template <typename WRITABLE>
     class ReadWriteContiguous : public  WRITABLE
     {
     public:
-        typedef typename WRITABLE::Type      Type;
-        typedef typename WRITABLE::ConstType ConstType;
+        //______________________________________________________________________
+        //
+        //
+        // Definitions
+        //
+        //______________________________________________________________________
+        typedef typename WRITABLE::Type      Type;      //!< alias
+        typedef typename WRITABLE::ConstType ConstType; //!< alias
 
         using WRITABLE::size;
         using WRITABLE::ask;
 
-        typedef Iter::Linear<Iter::Forward,Type>      Iterator;
-        typedef Iter::Linear<Iter::Reverse,Type>      ReverseIterator;
-        typedef Iter::Linear<Iter::Forward,ConstType> ConstIterator;
-        typedef Iter::Linear<Iter::Reverse,ConstType> ConstReverseIterator;
+        typedef Iter::Linear<Iter::Forward,Type>      Iterator;             //!< alias
+        typedef Iter::Linear<Iter::Reverse,Type>      ReverseIterator;      //!< alias
+        typedef Iter::Linear<Iter::Forward,ConstType> ConstIterator;        //!< alias
+        typedef Iter::Linear<Iter::Reverse,ConstType> ConstReverseIterator; //!< alias
 
+        //______________________________________________________________________
+        //
+        //
+        // C++
+        //
+        //______________________________________________________________________
 
+        //! setup
         inline explicit ReadWriteContiguous() :  WRITABLE()
         {
         }
 
+        //! cleanup
         inline virtual ~ReadWriteContiguous() noexcept
         {
         }
 
+        //______________________________________________________________________
+        //
+        //
+        // Methods
+        //
+        //______________________________________________________________________
+
+        //! \return legacy access
         inline Type * operator()(void)  noexcept
         {
             return (size()>0) ? (Type *)& ask(1) : 0;
         }
 
+        //! \return legacy const access
         inline ConstType * operator()(void)  const noexcept
         {
             return (size()>0) ? & ask(1) : 0;
         }
 
-        //
-        
-        inline Iterator begin() noexcept {
-            return (size()>0) ?  (Type *) &ask(1) : 0;
-        }
 
-        inline  Iterator end() noexcept
-        {
-            const size_t n = size();
-            return n>0 ? ((Type *)&ask(1))+n : 0;
-        }
+#if !defined(DOXYGEN_SHOULD_SKIP_THIS)
 
-        inline ReverseIterator rbegin() noexcept {
-            const size_t n = size();
-            return (n>0) ? ((Type *)&ask(n)) : 0;
-        }
+        inline Iterator        begin()  noexcept { return (size()>0) ?  (Type *) &ask(1) : 0; }
+        inline Iterator        end()    noexcept { const size_t n = size(); return n>0 ? ((Type *)&ask(1))+n : 0; }
+        inline ReverseIterator rbegin() noexcept { const size_t n = size(); return (n>0) ? ((Type *)&ask(n)) : 0; }
+        inline ReverseIterator rend()   noexcept { return (size() > 0) ? (((Type*)&ask(1))-1) : 0; }
 
-        inline ReverseIterator rend() noexcept
-        {
-            return (size() > 0) ? (((Type*)&ask(1))-1) : 0;
-        }
+        inline ConstIterator        begin()  const noexcept { return (size()>0) ? & ask(1) : 0; }
+        inline ConstIterator        end()    const noexcept { const size_t n = size(); return n>0 ? (&ask(1))+n : 0; }
+        inline ConstReverseIterator rbegin() const noexcept { const size_t n = size(); return (n>0) ? (&ask(n)) : 0; }
+        inline ConstReverseIterator rend()   const noexcept { return (size() > 0) ? ((&ask(1))-1) : 0; }
 
-        //
-        inline ConstIterator begin() const noexcept {
-            return (size()>0) ? & ask(1) : 0;
-        }
-
-        inline ConstIterator end() const noexcept
-        {
-            const size_t n = size();
-            return n>0 ? (&ask(1))+n : 0;
-        }
-
-        inline ConstReverseIterator rbegin() const noexcept {
-            const size_t n = size();
-            return (n>0) ? (&ask(n)) : 0;
-        }
-
-        inline ConstReverseIterator rend() const noexcept
-        {
-            return (size() > 0) ? ((&ask(1))-1) : 0;
-        }
-
+#endif // !defined(DOXYGEN_SHOULD_SKIP_THIS)
 
     private:
-        Y_Disable_Copy_And_Assign(ReadWriteContiguous);
+        Y_Disable_Copy_And_Assign(ReadWriteContiguous); //!< discarded
     };
 
 }
