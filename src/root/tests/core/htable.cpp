afd3a82bfdba6bcc45c1c932e4e97f5b0eb8d9cd
ybouret <yann.bouret@gmail.com>
2026-05-06 15:46:58 +0200

    removed bad htable

--- src/root/tests/core/htable.cpp
+++ src/root/tests/core/htable.cpp
@@ -10,61 +10,7 @@ Y_UTEST(core_htable)
 {
     Core::Rand ran;
 
-    Y_PRINTV(Core::HTable::BytesPerSlot);
-    Y_PRINTV(Core::HTable::MinTableSize);
-    Y_PRINTV(Core::HTable::MaxTableSize);
 
-#if 0
-    for(size_t bits=0;bits<=16;++bits)
-    {
-        size_t size = ran.gen<size_t>(bits);
-        std::cerr << size;
-        size_t numSlots = Core::HTable::SlotsFor(size);
-
-        std::cerr << " => " << size << " / #slot=" << numSlots;
-        std::cerr << std::endl;
-    }
-
-    {
-        size_t size     = Core::HTable::MaxTableSize;
-        size_t numSlots = Core::HTable::SlotsFor(size);
-        std::cerr << " => " << size << " / #slot=" << numSlots;
-
-    }
-#endif
-
-    Core::HTable table(10);
-    Y_PRINTV(table.size());
-    Y_PRINTV(table.capacity());
-    Vector<size_t> keys;
-    int            data = 7;
-
-    keys.free();
-    for(size_t i=table.capacity();i>0;--i)
-    {
-        GEN_KEY:
-        {
-            const size_t key = ran.gen<size_t>( ran.in<size_t>(0,sizeof(size_t)*8) );
-            for(size_t i=1;i<=keys.size();++i)
-            {
-                if(key == keys[i]) goto GEN_KEY;
-            }
-            keys << key;
-        }
-    }
-    Y_ASSERT(keys.size()==table.capacity());
-    //std::cerr << "keys=" << keys << std::endl;
-
-    for(size_t i=1;i<=keys.size();++i)
-    {
-        Y_ASSERT(table.insert(keys[i],&data));
-    }
-    Y_PRINTV(table.size());
-    Y_PRINTV(table.capacity());
-
-    table.reserve(4);
-    Y_PRINTV(table.size());
-    Y_PRINTV(table.capacity());
 
 }
 Y_UDONE()
--- src/root/y/core/htable.hpp
+++ src/root/y/core/htable.hpp
@@ -8,7 +8,6 @@
 #include "y/calculus/integer-log2.hpp"
 #include "y/config/setup.hpp"
 #include "y/container.hpp"
-#include "y/ability/expandable.hpp"
 
 namespace Yttrium
 {
@@ -16,45 +15,11 @@ namespace Yttrium
     namespace Core
     {
 
-        class HTable : public Expandable<Container>
+        class HTable
         {
         public:
             static const char * const CallSign; //!< "Core::HTable"
-            struct Slot {
-                size_t key;
-                void * args;
-            };
 
-            static const size_t ShiftPerSlot = IntegerLog2For<Slot>::Value;
-            static const size_t MaxLoadNumer = 3;
-            static const size_t MaxLoadDenom = 4;
-            static const size_t BytesPerSlot = sizeof(Slot);
-            static const size_t MaxPageBytes = Base2<size_t>::MaxBytes;
-            static const size_t MaxSlotCount = MaxPageBytes / BytesPerSlot;
-            static const size_t MinSlotCount = 4;
-            static const size_t MinTableSize = MaxLoadNumer * (MinSlotCount/MaxLoadDenom);
-            static const size_t MaxTableSize = MaxLoadNumer * (MaxSlotCount/MaxLoadDenom);
-            static size_t SlotsFor(size_t &minCapacity);
-
-
-            explicit HTable(const size_t minCapacity = 0);
-            virtual ~HTable() noexcept;
-
-            virtual size_t size()     const noexcept;
-            virtual size_t capacity() const noexcept;
-
-            const void * search(const size_t key) const noexcept;
-            void *       search(const size_t key) noexcept;
-            bool         remove(const size_t key) noexcept;
-            
-
-            bool insert(const size_t key, void * const args);
-            void reserve(const size_t n);
-
-        private:
-            Y_Disable_Copy_And_Assign(HTable);
-            class Code;
-            Code * const code;
         };
 
     }
