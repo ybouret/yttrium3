98b68c0d2a6dad74fe303943381440bd915a9b03
ybouret <yann.bouret@gmail.com>
2026-05-11 17:04:31 +0200

    need to define Optimizing flag and test

--- src/root/tests/coven/tribes.cpp
+++ src/root/tests/coven/tribes.cpp
@@ -142,6 +142,15 @@ Y_UTEST(coven_tribes)
         Print(num4,vec4);
         Y_ASSERT(vec4==vec0);
 
+
+        (std::cerr << "-- use Optimizing: ").flush();
+
+        Coven::StandardSurvey vec5;
+        const apn             num5 = TestTribes(mu,
+                                                Coven::Tribes::HyperPlane | Coven::Tribes::NoColinear | Coven::Tribes::NoMultiple,
+                                                vc,rc,xml,vec5);
+        Print(num5,vec5);
+        Y_ASSERT(vec5==vec0);
         std::cerr << "-- done" << std::endl;
     }
 
