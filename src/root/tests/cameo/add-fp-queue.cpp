6bc6fc7f34f5e344e151258ead7b2222185789fd
Yann Bouret <yann.bouret@unice.fr>
2026-05-15 15:19:53 +0200

    updated test with fp queue

--- src/root/tests/cameo/add-fp-queue.cpp
+++ src/root/tests/cameo/add-fp-queue.cpp
@@ -7,14 +7,22 @@ using namespace Yttrium;
 Y_UTEST(cameo_add_fp_queue)
 {
 
+    {
+        Cameo::FP_QueueSummator<float> fpq;
+        std::cerr << fpq << std::endl;
+        fpq << 1.2f << -2.7f << 0.01f;
+        std::cerr << fpq << std::endl;
+        std::cerr << fpq() << std::endl;
+    }
 
-    Cameo::FP_QueueSummator<float> fpq;
+    {
+        Cameo::FP_QueueSummator< XReal<double> > fpq;
+        std::cerr << fpq << std::endl;
+        fpq << 1.2f << -2.7f << 0.01f;
+        std::cerr << fpq << std::endl;
+        std::cerr << fpq() << std::endl;
 
-    std::cerr << fpq << std::endl;
-    fpq << 1.2f << -2.7 << 0.01f;
-    std::cerr << fpq << std::endl;
-
-    std::cerr << fpq() << std::endl;
+    }
 
 }
 Y_UDONE()
