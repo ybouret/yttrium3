1596a638d5bd98ab55ede2923569c3eb98ae94bf
ybouret <yann.bouret@gmail.com>
2026-05-20 08:33:58 +0200

    fixing from win32, gcc

--- src/root/y/hashing/key/dumper.cpp
+++ src/root/y/hashing/key/dumper.cpp
@@ -10,9 +10,10 @@ namespace Yttrium
         template <> template<>
         uint16_t Dumping<uint16_t>::  From<uint8_t>(const uint8_t &x) noexcept
         {
+            static const uint16_t u8max = 255;
             const uint16_t lo = x;
-            const uint16_t hi = ~x;
-            return lo | (hi<<8);
+            const uint16_t hi = (uint16_t)(u8max-x);
+            return (uint16_t)(lo | (hi<<8));
         }
 
     }
