6ded425db37bb73940bc11d60285eb80566926e9
ybouret <yann.bouret@gmail.com>
2026-05-21 20:35:19 +0200

    working on lexer

--- src/root/y/jive/lexer.cpp
+++ src/root/y/jive/lexer.cpp
@@ -59,6 +59,13 @@ namespace Yttrium
         Lexeme * Lexer:: get(Source &source)
         {
             assert(curr);
+            //------------------------------------------------------------------
+            //
+            //
+            // GET: test existing lexeme of probe...
+            //
+            //
+            //------------------------------------------------------------------
         GET:
             if(lexemes.size)
             {
@@ -66,47 +73,70 @@ namespace Yttrium
             }
             else
             {
+                //--------------------------------------------------------------
+                //
+                //
+                // PROBE: new lexeme
+                //
+                //
+                //--------------------------------------------------------------
             PROBE:
                 Lexical::Command cmd;
                 Lexeme * const   lxm = curr->probe(source,cmd);
                 if(lxm)
                 {
+                    //----------------------------------------------------------
+                    //
+                    // found a regular lexeme
+                    //
+                    //----------------------------------------------------------
                     return lxm;
                 }
                 else
                 {
+                    //----------------------------------------------------------
+                    //
+                    // found a command lexeme
+                    //
+                    //----------------------------------------------------------
                     const char * const here = curr->name->c_str();
                     switch(cmd.kind)
                     {
+
+                            //--------------------------------------------------
                         case Lexical::Command::Quit:
-                            std::cerr << "[EOS]" << std::endl;
+                            //--------------------------------------------------
                             switch(curr->onEOS)
                             {
-                                case Lexical::AcceptEOS:
-                                    return 0; // EOS
+                                case Lexical::AcceptEOS: return 0; // EOS: normal return
                                 case Lexical::RejectEOS: {
+                                    // illegal stop
                                     Specific::Exception excp(name->c_str(), "End Of Stream in <%s>", here);
                                     throw source->stamp(excp);
                                 }
                             }
                             break;
 
+                            //--------------------------------------------------
                         case Lexical::Command::Call: {
+                            //--------------------------------------------------
                             assert(0!=cmd.args);
+                            // search scanner to call
                             const String     id = *cmd.args; std::cerr << "call  to  <" << id << ">" << std::endl;
                             PScanner * const ps = psdb.search(id);
-                            if(!ps)
-                            {
+                            if(!ps) {
                                 Specific::Exception excp(name->c_str(),"<%s> got no <%s> to call", here, id.c_str());
                                 throw source->stamp(excp);
                             }
+                            // modify history and current scanner
                             history << *curr;
                             curr = & **ps;
                             if(Plugin * const plugin = dynamic_cast<Plugin *>(curr)) plugin->enter();
+                        } goto PROBE; // still no lexeme
 
-                        } goto PROBE;
-
+                            //--------------------------------------------------
                         case Lexical::Command::Back: {
+                            //--------------------------------------------------
                             assert(0==cmd.args);  std::cerr << "back from <" << curr->name << ">" << std::endl;
                             if(history->size<=0) {
                                 Specific::Exception excp(name->c_str(),"no possible coming back from <%s>", here);
@@ -114,7 +144,7 @@ namespace Yttrium
                             }
                             if(Plugin * const plugin = dynamic_cast<Plugin *>(curr)) plugin->leave();
                             curr = & history.tail(); history.popTail();
-                        } goto GET;
+                        } goto GET; // may have created a lexeme
 
                         default:
                             throw Exception("command not processed yet");
