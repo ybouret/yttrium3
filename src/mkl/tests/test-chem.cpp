5c728f0dff57057cf5200703cd1458806d8e2507
ybouret <yann.bouret@gmail.com>
2026-05-23 09:56:59 +0200

    fixed ortho space

--- src/mkl/tests/test-chem.cpp
+++ src/mkl/tests/test-chem.cpp
@@ -12,26 +12,40 @@ Y_UTEST(chem)
     Matrix<apz> Q;
 
     {
+        // H+ HO-
         Matrix<int> nu(1,2);
         nu[1][1] = 1; nu[1][2] = 1;
         std::cerr << "nu=" << nu << std::endl;
         Y_ASSERT(MKL::OrthoSpace::Get(Q,nu));
         std::cerr << "Q=" << Q << std::endl;
-        Coven::Inquiry<Coven::PositiveSurvey> survey(nu,Coven::InquiryIncludesInitial);
-        survey->print(std::cerr);
+        Coven::Inquiry<Coven::PositiveSurvey> survey(Q,Coven::InquiryIncludesInitial);
+        survey->print(std::cerr << "-- ortho: " << std::endl);
         //rawPosInq.output(xml,nr);
     }
 
     {
+        // H+ HO- AH A-
         Matrix<int> nu(2,4);
         nu[1][1] = 1; nu[1][2] = 1;
         nu[2][1] = 1; nu[2][3] = -1; nu[2][4] = 1;
         std::cerr << "nu=" << nu << std::endl;
         Y_ASSERT(MKL::OrthoSpace::Get(Q,nu));
         std::cerr << "Q=" << Q << std::endl;
-        Coven::Inquiry<Coven::PositiveSurvey> survey(nu,Coven::InquiryIncludesInitial);
-        survey->print(std::cerr);
+        Coven::Inquiry<Coven::PositiveSurvey> survey(Q,Coven::InquiryIncludesInitial);
+        survey->print(std::cerr << "-- ortho: " << std::endl);
+    }
 
+    {
+        // H+ HO- AH2 AH- A2-
+        Matrix<int> nu(3,5);
+        nu[1][1] = 1; nu[1][2] = 1;
+        nu[2][1] = 1; nu[2][3] = -1; nu[2][4] = 1;
+        nu[3][1] = 1; nu[3][4] = -1; nu[3][5] = 1;
+        std::cerr << "nu=" << nu << std::endl;
+        Y_ASSERT(MKL::OrthoSpace::Get(Q,nu));
+        std::cerr << "Q=" << Q << std::endl;
+        Coven::Inquiry<Coven::PositiveSurvey> survey(Q,Coven::InquiryIncludesInitial);
+        survey->print(std::cerr << "-- ortho: " << std::endl);
     }
 
 
--- src/mkl/y/mkl/algebra/ortho-space.cpp
+++ src/mkl/y/mkl/algebra/ortho-space.cpp
@@ -18,6 +18,9 @@ namespace Yttrium
             const apz   dP2 =     lu.determinant(P2); if(dP2.is0()) { Q.release(); return false;}
             Matrix<apz> aP2(n,n); lu.adjoint(aP2,P2);
 
+            std::cerr << "P2=" << P2 << std::endl;
+            std::cerr << "aP2=" << aP2 << std::endl;
+
             Q.make(m,m);
             {
                 Matrix<apz>       P3(n,m);P3.mmul(aP2,P);
@@ -28,8 +31,8 @@ namespace Yttrium
             for(size_t i=m;i>0;--i)
             {
                 Q[i][i] = dP2 - Q[i][i];
-                for(size_t j=m;j>i;--j)   Sign::MakeOpposite( Coerce(Q[i][i].s) );
-                for(size_t j=i-1;j>0;--j) Sign::MakeOpposite( Coerce(Q[i][i].s) );
+                for(size_t j=m;j>i;--j)   Sign::MakeOpposite( Coerce(Q[i][j].s) );
+                for(size_t j=i-1;j>0;--j) Sign::MakeOpposite( Coerce(Q[i][j].s) );
             }
             
             return true;
