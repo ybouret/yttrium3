c94c2b8147ae891c94b9932ca2667e801f546c90
ybouret <yann.bouret@gmail.com>
2026-06-03 12:14:05 +0200

    wip

--- src/root/y/jive/syntax/rule/compound/alternate.cpp
+++ src/root/y/jive/syntax/rule/compound/alternate.cpp
@@ -22,7 +22,25 @@ namespace Yttrium
 
             Y_Jive_Syntax_Rule_Impl(Alternate)
             {
+                const Nesting nesting(framework);
+                Outcome       outcome(Rejected,Healthy,Running);
+
+                for( const RNode * node = list.head;node;node=node->next)
+                {
+                    const Outcome rout = (**node).accepts(framework);
+                    if(Accepted==rout.result)
+                    {
+                        outcome.result = Accepted;
+                        if( Healthy == (outcome.sanity = rout.sanity) )
+                        {
+                            break;
+                        }
+                    }
+                }
+
                 
+
+                return outcome;
             }
         }
 
