c40ed9b6b7af681db920a04013191f6fe5d700da
ybouret <yann.bouret@gmail.com>
2026-06-13 16:50:54 +0200

    wip actors

---
+++ src/chem/y/chemical/reactive/actors.hpp
@@ -0,0 +1,23 @@
+
+
+//! \file
+
+#ifndef Y_Chemical_Actors_Included
+#define Y_Chemical_Actors_Included 1
+
+#include "y/chemical/reactive/actor.hpp"
+
+namespace Yttrium
+{
+    namespace Chemical
+    {
+
+        class Actors
+        {
+        };
+
+    }
+}
+
+#endif // !Y_Chemical_Actors_Included
+
