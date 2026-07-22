a5c43260aaafc755a78f745ee224fc666186811c
ybouret <yann.bouret@gmail.com>
2026-07-22 18:11:52 +0200

    formula

--- src/root/tests/concurrent/updiag.cpp
+++ src/root/tests/concurrent/updiag.cpp
@@ -2,6 +2,7 @@
 #include "y/utest/run.hpp"
 
 #include "y/calculus/isqrt.hpp"
+#include "y/calculus/alignment.hpp"
 
 namespace Yttrium
 {
@@ -14,21 +15,62 @@ namespace Yttrium
             {
             public:
 
+                explicit UpperDiagonalTile(const size_t extent) noexcept;
 
+                virtual ~UpperDiagonalTile() noexcept;
 
-                static size_t GetI(const size_t k) noexcept
-                {
-                    const size_t D = (k<<3)-3;
-                    const size_t S = IntegerSquareRoot(D);
-                    return (1+S)>>1;
-                }
+                size_t getRow(const size_t k) const noexcept;
+                size_t getCol(const size_t k, const size_t i) const noexcept;
 
-
-
+                const size_t n;        //!< n x n array
+                const size_t B;        //!< 1 + 2*n*
+                const size_t B2;       //!< B^2
+                const size_t kNumber;  //!< n*(n+1)/2
+                const size_t kOffset;  //!< initial valid k
+                const size_t kLength;  //!< number of indices, 0 meanms empty
+                const size_t kUtmost;  //!< utmost  valid k
+                //const size_t h;        //!< height
 
             private:
                 Y_Disable_Copy_And_Assign(UpperDiagonalTile);
             };
+
+
+            UpperDiagonalTile:: UpperDiagonalTile(const size_t extent) noexcept :
+            n(extent),
+            B(1+(n<<1)),
+            B2(B*B),
+            kNumber( (n*(n+1)>>1) ),
+            kOffset(1),
+            kLength(kNumber),
+            kUtmost(kOffset + kLength - 1 )
+            {
+            }
+
+            UpperDiagonalTile:: ~UpperDiagonalTile() noexcept
+            {
+            }
+
+            size_t UpperDiagonalTile:: getRow(const size_t k) const noexcept
+            {
+                assert(k>=1);
+                assert(k<=kNumber);
+                const size_t Delta = B2 - (k<<3);
+                const size_t twice = (B - IntegerSquareRoot(Delta));
+                const size_t align = Alignment::On<2>::Ceil(twice);
+                return align>>1;
+            }
+
+            size_t UpperDiagonalTile:: getCol(const size_t k, const size_t i) const noexcept
+            {
+                assert(k>=1);
+                assert(k<=kNumber);
+                const size_t im1 = i-1;
+                const size_t kmx = im1*(n+1) - ( (i*im1)>>1 );
+                return im1 + k-kmx;
+            }
+
+
         }
     }
 
@@ -39,23 +81,17 @@ using namespace Yttrium;
 Y_UTEST(concurrent_updiag)
 {
 
-    for(size_t n=1;n<=5;++n)
+    for(size_t n=1;n<=4;++n)
     {
-        std::cerr << std::endl;
-        const size_t kmax = (n*(n+1))/2;
-        std::cerr << "n=" << n << " => kmax=" << kmax << std::endl;
+        std::cerr << std::endl << "n=" << n << std::endl;
+        const size_t kmax = (n*(n+1))>>1;
+        Concurrent:: Splitting:: UpperDiagonalTile udt(n);
         for(size_t k=1;k<=kmax;++k)
         {
-            std::cerr << "k=" << k;
-            const size_t i = Concurrent::Splitting::UpperDiagonalTile::GetI(k);
-            std::cerr << " | i=" << i;
-            const size_t j = i + (k-(1+i*(i-1)/2));
-            std::cerr << " | j=" << j;
-
-            std::cerr << std::endl;
-
+            const size_t r = udt.getRow(k);
+            const size_t c = udt.getCol(k,r);
+            std::cerr << "\tk=" << std::setw(2) << k << " : r=" << r << " : c=" << c << std::endl;
         }
-
 
     }
 
