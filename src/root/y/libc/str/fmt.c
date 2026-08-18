b516aee1e566bf2edd6ea0fe154ee67e9bf3e9a1
ybouret <yann.bouret@gmail.com>
2026-08-18 11:31:42 +0200

    wip

--- src/root/y/libc/str/fmt.c
+++ src/root/y/libc/str/fmt.c
@@ -16,6 +16,9 @@ int Yttrium_Strfmt(char * const       buffer,
     memset(buffer,0,buflen);
     {
         const int res = vsnprintf(buffer,buflen,fmt,*app);
+        fflush(stdout);
+        printf("strfmt error: %d\n", res);
+        fflush(stdout);
         if(res<0) memset(buffer,0,buflen);
         return res;
     }
