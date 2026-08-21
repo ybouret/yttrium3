f68c1f30e9c7245fd096dfad1755c5f46e4b9f6e
ybouret <yann.bouret@gmail.com>
2026-08-21 08:04:08 +0200

    working on tao, need to factorize code...

---
+++ src/mkl/y/mkl/tao/2.hpp
@@ -0,0 +1,201 @@
+//! \file
+
+#ifndef Y_MKL_Tao2_Included
+#define Y_MKL_Tao2_Included 1
+
+#include "y/mkl/tao/device.hpp"
+#include "y/container/matrix.hpp"
+
+namespace Yttrium
+{
+    namespace MKL
+    {
+
+        namespace Tao
+        {
+
+            namespace Pith
+            {
+                //______________________________________________________________
+                //
+                //
+                //
+                //! Handle Matrix/Vector common operations
+                //
+                //
+                //______________________________________________________________
+                template <
+                typename TARGET,
+                typename T,
+                typename SOURCE,
+                typename U>
+                struct MulOps
+                {
+                    typedef Cameo::Addition<U> XAdd;    //!< alias
+                    TARGET                   * target;  //!< target vector address
+                    const Matrix<T>          * matrix;  //!< matrix address
+                    SOURCE                   * source;  //!< source vector address
+                    Tiles1D                  * tiles1d; //!< device tiles1d address
+
+                    //! target = matrix*source \param ctx context to fetch tile1d
+                    inline void set(Concurrent::Context &ctx)
+                    {
+                        assert(target);
+                        assert(matrix);
+                        assert(source);
+                        assert(tiles1d);
+
+                        const Tile1D &tile = (*tiles1d)[ctx.indx];
+                        XAdd &       xadd  = *tile.as<XAdd *>();
+
+                        for(size_t irow=tile.offset,count=tile.length;count>0;--count,++irow)
+                            (*target)[irow] = matrix->mul_(irow,xadd,*source);
+
+                    }
+
+                    //! target = matrix*source + rhs \param ctx context to fetch tile1d \param rhs to add
+                    template <typename RHS>
+                    inline void add(Concurrent::Context &ctx, RHS &rhs)
+                    {
+                        assert(target);
+                        assert(matrix);
+                        assert(source);
+                        assert(tiles1d);
+
+                        const Tile1D &tile = (*tiles1d)[ctx.indx];
+                        XAdd &       xadd  = *tile.as<XAdd *>();
+                        for(size_t irow=tile.offset,count=tile.length;count>0;--count,++irow)
+                            (*target)[irow] = matrix->muladd_(irow,xadd,*source,rhs);
+
+                    }
+
+                    //! target = matrix*source-rhs \param ctx context to fetch tile1d \param rhs to sub
+                    template <typename RHS>
+                    inline void sub(Concurrent::Context &ctx, RHS &rhs)
+                    {
+                        assert(target);
+                        assert(matrix);
+                        assert(source);
+                        assert(tiles1d);
+
+                        const Tile1D &tile = (*tiles1d)[ctx.indx];
+                        XAdd &       xadd  = *tile.as<XAdd *>();
+                        for(size_t irow=tile.offset,count=tile.length;count>0;--count,++irow)
+                            (*target)[irow] = matrix->mulsub_(irow,xadd,*source,rhs);
+
+                    }
+
+
+                };
+
+            }
+
+            template <
+            typename TARGET,
+            typename T,
+            typename SOURCE,
+            typename U>
+            inline void Mul(Device            & device,
+                            TARGET            & target,
+                            const Matrix<T>   & matrix,
+                            SOURCE            & source,
+                            Cameo::Addenda<U> & addenda)
+            {
+                assert(target.size() == matrix.rows);
+                assert(source.size() == matrix.cols);
+
+                // get metrics
+                const size_t nr = matrix.rows;
+                const size_t nc = matrix.cols;
+
+                // prepare device
+                device.remap1d(nr).attach(addenda,nc);
+
+                // prepare ops
+                typedef Pith::MulOps<TARGET,T,SOURCE,U> MulOps;
+                MulOps ops =
+                {
+                    & target, & matrix, & source , & device.tiles1d
+                };
+
+                // apply ops on each tile
+                (*device)( ops, & MulOps :: set);
+            }
+
+            template <
+            typename TARGET,
+            typename T,
+            typename SOURCE,
+            typename U,
+            typename RHS>
+            inline void MulAdd(Device            & device,
+                               TARGET            & target,
+                               const Matrix<T>   & matrix,
+                               SOURCE            & source,
+                               RHS               & rhs,
+                               Cameo::Addenda<U> & addenda)
+            {
+                assert(target.size() == matrix.rows);
+                assert(source.size() == matrix.cols);
+
+                // get metrics
+                const size_t nr = matrix.rows;
+                const size_t nc = matrix.cols;
+
+                // prepare device
+                device.remap1d(nr).attach(addenda,nc);
+
+                // prepare ops
+                typedef Pith::MulOps<TARGET,T,SOURCE,U> MulOps;
+                MulOps ops =
+                {
+                    & target, & matrix, & source , & device.tiles1d
+                };
+
+                // apply ops on each tile
+                (*device)( ops, & MulOps :: template add<RHS>, rhs);
+            }
+
+            template <
+            typename TARGET,
+            typename T,
+            typename SOURCE,
+            typename U,
+            typename RHS>
+            inline void MulSub(Device            & device,
+                               TARGET            & target,
+                               const Matrix<T>   & matrix,
+                               SOURCE            & source,
+                               RHS               & rhs,
+                               Cameo::Addenda<U> & addenda)
+            {
+                assert(target.size() == matrix.rows);
+                assert(source.size() == matrix.cols);
+
+                // get metrics
+                const size_t nr = matrix.rows;
+                const size_t nc = matrix.cols;
+
+                // prepare device
+                device.remap1d(nr).attach(addenda,nc);
+
+                // prepare ops
+                typedef Pith::MulOps<TARGET,T,SOURCE,U> MulOps;
+                MulOps ops =
+                {
+                    & target, & matrix, & source , & device.tiles1d
+                };
+
+                // apply ops on each tile
+                (*device)( ops, & MulOps :: template sub<RHS>, rhs);
+            }
+
+
+
+        }
+
+    }
+}
+
+#endif // !Y_MKL_Tao2_Included
+
