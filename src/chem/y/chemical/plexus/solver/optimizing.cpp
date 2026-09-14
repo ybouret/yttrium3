80793dce62d0d1aa68e96e8f515913c82ac1a353
ybouret <yann.bouret@gmail.com>
2026-09-14 09:38:37 +0200

    need to track singularity in Affinity evaluation

--- src/chem/y/chemical/plexus/solver/optimizing.cpp
+++ src/chem/y/chemical/plexus/solver/optimizing.cpp
@@ -1,6 +1,6 @@
 #include "y/chemical/plexus/solver.hpp"
 #include "y/stream/libc/output.hpp"
-
+#include "y/type/temporary.hpp"
 
 namespace Yttrium
 {
@@ -17,20 +17,20 @@ namespace Yttrium
             // initialize Cend at 1D solution
             //
             //------------------------------------------------------------------
+            const String &eid = a.eq.name;
+            Y_XML_Element_Attr(xml,Optimizing, Y_XML_Attr(eid) );
             Solver &self = *this;
             Cend.load(a.cc);
             const xreal_t  F1 = F(Cend,SubLevel);
-            Y_XMLog(xml,"F(0) = " << self(0).str() << "/" << F0.str() );
-            Y_XMLog(xml,"F    = " << F1.str() <<  " @" << a.eq);
-            Y_XMLog(xml,"F(1) = " << self(1).str() );
+            Y_XMLog(xml,"F(0)   = " << self(0).str() << " / " << F0.str() );
+            Y_XMLog(xml,"F(1)   = " << F1.str()      << " / " << self(1).str() );
 
 
             if(Trace)
             {
                 const String fn = MakeFileName(a.eq.name) + ".ycp";
                 OutputFile   fp(fn);
-                saveProfile(fp,500);
-
+                saveProfile(fp,1000);
                 {
                     if(1==i)
                         trace += "plot ";
@@ -40,17 +40,40 @@ namespace Yttrium
                 }
             }
 
-            
+
             // study cases
-            XTriplet xx = {  0, 0.5,         1 };
+            XTriplet xx = {  0, MKL::Numeric<xreal_t>::HALF, 1 };
             XTriplet ff = { F0, self(xx.b), F1 };
 
-            Y_XMLog(xml,ff.a.str() << " -> " << ff.b.str() << " -> " << ff.c.str() );
+            Y_XMLog(xml,"F(1/2) = " << ff.b.str() );
 
-            const xreal_t x_opt = opt.find(xml,self,Minimize::Inside,xx,ff,Minimize::Standard);
+            if(F1<F0)
+            {
+                Y_XMLog(xml, "[+]");
+                // directly ok!
+                if(ff.b<F1)
+                {
+                    const bool    flag = xml.verbose; //xml.verbose = false;
+                    const xreal_t x_opt = opt.find(xml,self,Minimize::Direct,xx,ff,Minimize::Standard);
+                    xml.verbose = flag;
+                    Y_XMLog(xml, "[++] F(" << x_opt.str() <<") = " << ff.b );
+                }
+
+
+            }
+            else
+            {
+                assert(F1>=F0);
+                Y_XMLog(xml, "[-]");
+
+            }
 
 
 
+
+            return true;
+
+            //const xreal_t x_opt = opt.find(xml,self,Minimize::Inside,xx,ff,Minimize::Standard);
 
             return true;
         }
--- src/mkl/y/mkl/minimize/api.cpp
+++ src/mkl/y/mkl/minimize/api.cpp
@@ -71,7 +71,6 @@ namespace Yttrium
                     ++cycle;
                     Y_XMLog(xml, "[cycle=" << cycle << "]");
                     step(xml,x,f,F);
-                    std::cerr << "\t\tf=" << f << std::endl;
                     const T    x_new = x.b;
                     const bool f_cvg = AlmostEqual<T>::Are(f.a,f.b) && AlmostEqual<T>::Are(f.b,f.c);
                     const bool x_cvg = AlmostEqual<T>::Are(x_opt,x_new);
